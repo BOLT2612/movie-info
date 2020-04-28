d38e942fbfd5067c35728bb42b3f8b961f36fde3
Greg Coffeng <gcoffeng@gmail.com>
2020-04-28 15:03:09 -0700

    port cannot be called anything but PORT for heroku

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname,'../dist')));
 app.use(router);
 
-const port = process.env.MOVIE_INFO_PORT;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   time = new Date().toLocaleTimeString()
